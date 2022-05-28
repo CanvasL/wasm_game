06597df491cc34a7ff9bf1065fb1a15c8a8bacf1
CanvasL <746591811@qq.com>
2022-05-28 18:05:10 +0800

    feat: map painted by canvas

--- www/src/index.js
+++ www/src/index.js
@@ -1,6 +1,30 @@
-import init, {hello} from "wasm_game"
+import init, {
+    World
+} from 'wasm_game'
 
 init().then(() => {
-    hello("Canvas");
-    console.log("OK")
+    const CELL_SIZE = 20
+    const CELL_AMOUNT = 16;
+    const world = World.new(CELL_AMOUNT)
+    const worldWidth = world.width()
+
+    const canvas = document.getElementById('snake-world')
+    const context = canvas.getContext('2d')
+
+    canvas.width = worldWidth * CELL_SIZE
+    canvas.height = worldWidth * CELL_SIZE
+
+    function drawWorld() {
+        context.beginPath()
+        for (let x = 0; x < worldWidth + 1; x++) {
+            context.moveTo(CELL_SIZE * x, 0)
+            context.lineTo(CELL_SIZE * x, CELL_SIZE * worldWidth)
+        }
+        for (let y = 0; y < worldWidth + 1; y++) {
+            context.moveTo(0, CELL_SIZE * y)
+            context.lineTo(CELL_SIZE * worldWidth, CELL_SIZE * y)
+        }
+        context.stroke();
+    }
+    drawWorld();
 })
